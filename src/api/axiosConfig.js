47d0fd02b7fe065e389d1a6e3f413aaae8dba478
Ravza <ravzasisiknur@gmail.com>
2025-05-28 18:36:39 +0300

    b

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: 'http://75.119.144.130/api',
+  baseURL: 'https://75.119.144.130/api',
   headers: {
     'Content-Type': 'application/json',
     ...(token && { Authorization: `Bearer ${token}` })
