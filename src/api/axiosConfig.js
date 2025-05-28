66a3d5e6865a0eb8ce4d0eaa5a8eec95346fdd01
Ravza <ravzasisiknur@gmail.com>
2025-05-28 10:25:35 +0300

    fix : backend link

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = localStorage.getItem('token');
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:5000/api',
+  baseURL: 'https://75.119.144.130:5000/api',
   headers: {
     'Content-Type': 'application/json',
     ...(token && { Authorization: `Bearer ${token}` })
